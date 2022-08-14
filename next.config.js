d7ec1a6df00272ad724cf9fcec57a6fe999ed9d3
FUTURE\matsuoka3548 <y.matsuoka.dz@future.co.jp>
2022-08-14 18:50:28 +0900

    auth commit2

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  env: {
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
+  }
+}
